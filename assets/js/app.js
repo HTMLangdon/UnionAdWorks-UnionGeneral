50dd7624b665db9755124f2ddaffcb82498f81c7
HTMLangdon <HTMLangdon@users.noreply.github.com>
2016-06-30 18:19:30 -0400

    -= MAKE SURE LAST VERSION ISNT BETTER!=-

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,9 +8,13 @@ function initCupcallery() {
             gallery: {
                 enabled: true
             }
+
         });
+
     });
+
 }
+
 
 function onSiteImagesLoadComplete() {
     $('img').error(function () {
@@ -19,58 +23,34 @@ function onSiteImagesLoadComplete() {
 }
 
 function setupFullBrowserSections() {
-    //    $("header a.nav-title").click(function (e) {
-    //
-    //        console.log($(this.hash).offset().top);
-    //        console.log(this.hash);
-    //        console.log($(this));
-    //
-    //        console.log($(this.hash))
-    //
-    //console.log("-----------");
-    //        e.preventDefault();
-    //        $('html,body').animate({
-    //            //            scrollTop: $(this.hash).offset().top}, 1000)
-    //            scrollTop: $(this.hash).offset().top
-    //        }, 1000);
-    //    });
-    //
-
 
     count = $("nav.navigation-main ul").children().length;
 
-
     $('.fullBrowserSections').fullpage({
+        csss3: true,
+        fitToSection:false,
         autoScrolling: false
+
     });
 
 }
 
 
 
-function removeNonIndexedSections() {
-    //    $('.about-b').removeClass('fp-section');
-    //    $('.cupcakery').removeClass('fp-section');
-}
+function removeNonIndexedSections() {}
 
 function initMainMenu() {
-    // If a link has a dropdown, add sub menu toggle.xxz
-    //        $('nav ul li a:not(:only-child)').click(function (e) {
-    //            $(this).siblings('.nav-dr>e/opdown').toggle();
-
-
-    //            // Close one dropdown when selecting another
-    //            $('.nav-dropdown').not($(this).siblings()).hide();
-    //            e.stopPropagation();
-    //        });
     // Clicking away from dropdown will remove the dropdown class
     $('html').click(function () {
         $('.nav-dropdown').hide();
     });
+    
     // Toggle open and close nav styles on click
     $('#btn-hamurberX').click(function () {
         $('nav ul').slideToggle();
     });
+    
+    
     // Hamburger to X toggle
     $('#btn-hamurberX').on('click', function () {
         this.classList.toggle('active');
